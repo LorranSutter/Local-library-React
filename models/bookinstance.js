0c798fc6f97025fb6bb47be2f6988dfed3eab791
Lorran Sutter <sutterlorran@gmail.com>
2020-02-17 16:07:03 -0500

    Book instance list

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -16,6 +17,12 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 //Export model
